39dee8a292e1f7c7ab9e97bfd7824e1679494035
ccankayali <volkand135@gmail.com>
2024-02-21 18:44:35 +0300

    ai

--- backend/src/ai/ai.module.ts
+++ backend/src/ai/ai.module.ts
@@ -1,4 +1,15 @@
 /* eslint-disable prettier/prettier */
 export class AiModule {
+    getAnswer(question: string): string | PromiseLike<string> {
+        if(question === "How are you?") {
+            return "I'm good, thank you!";
+        }
 
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve("This is an async answer to your question."), 1000);
+        });
+    }
 }
+
+// ! DÜZENELENECEK
