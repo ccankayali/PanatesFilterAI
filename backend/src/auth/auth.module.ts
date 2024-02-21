80cb9ab26b92689a1d18eb4cb6b7363efed388ce
ccankayali <volkand135@gmail.com>
2024-02-21 10:45:07 +0300

    auth

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+// /src/auth/auth.module.ts
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersService } from './usersService';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/user.entity';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'process.env.JWT_SECRET',
+      signOptions: { expiresIn: '60m' },
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [AuthService, JwtStrategy, UsersService],
+  exports: [AuthService],
+})
+export class AuthModule {}
