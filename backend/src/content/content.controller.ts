8d983b721387671fbd7145d54d629054115432a5
ccankayali <volkand135@gmail.com>
2024-02-23 10:08:34 +0300

    content //full

--- backend/src/content/content.controller.ts
+++ backend/src/content/content.controller.ts
@@ -7,18 +7,18 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { ContentService } from './content.service';
+import { CheckContentDto } from './dto/check-content.dto';
 
 @Controller('content-check')
 export class ContentController {
   constructor(private contentCheckService: ContentService) {}
 
   @Post()
-  async check(@Body('content') content: string) {
-    const contentDto = { content };
-    const isContentValid = await this.contentCheckService.checkContent(contentDto); // contentDto'yu argüman olarak kullan
-    if (!isContentValid) {
-      throw new HttpException('Content is not valid', HttpStatus.BAD_REQUEST);
-    }
-    return { message: 'Content is valid' };
+  async check(@Body() dto: CheckContentDto) {
+  const isContentValid = await this.contentCheckService.checkContent(dto);
+  if(isContentValid){
+    throw new HttpException('Content is not valid', HttpStatus.BAD_REQUEST);
+  }
+  return { message: 'Content is valid'};
   }
 }
--- backend/src/content/content.service.ts
+++ backend/src/content/content.service.ts
@@ -5,15 +5,11 @@ import { CheckContentDto } from './dto/check-content.dto';
 
 @Injectable()
 export class ContentService {
-    // DTO'yu metot parametresi olarak kullanıyoruz ve içindeki content özelliğini kontrol ediyoruz
     async checkContent(dto: CheckContentDto): Promise<string> {
-        // Basit bir içerik doğrulama örneği
         if (!dto.content || dto.content.trim().length === 0) {
             throw new Error('Content is empty or not provided.');
         }
         
-        // İçerik geçerli ise, bir işlem yapın veya bir değer döndürün
-        // Bu örnekte basitçe bir string döndürüyoruz
         return 'Content is valid and processed.';
     }
     constructor(private aiService: AiModule) {}
