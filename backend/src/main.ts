debf234e11e782c2e8567ba26a7dafc0adcd7d88
ccankayali <volkand135@gmail.com>
2024-02-23 11:52:47 +0300

    main

---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api');
+  
+  await app.listen(5000);
+  
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+bootstrap();
