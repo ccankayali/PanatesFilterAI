bd17037f96d22f7ab53af8bfaba7fdaccec2501d
ccankayali <volkand135@gmail.com>
2024-02-23 11:52:27 +0300

    app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PostModule } from './post/post.module';
+import { ContentModule } from './content/content.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo:27017/mydatabase')],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    PostModule,
+    ContentModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
