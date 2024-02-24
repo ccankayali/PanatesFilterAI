7e3cfd67952053b0dbc14d55fa2e5115bbddf0bf
ccankayali <volkand135@gmail.com>
2024-02-24 13:23:03 +0300

    controller  //full

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersService } from './usersService';
+import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+    constructor(
+        private authService: AuthService,
+        private usersService: UsersService,
+    ) {}
 
-  @Post('login')
-  @HttpCode(HttpStatus.OK)
-  async login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto);
-  }
+    @Post('signup')
+    async signup(@Body() signupDto: SignupDto) {
+        const user = await this.usersService.create(signupDto.email, signupDto.password);
+        return {
+            message: "User created successfully",
+            userId: user.id,
+            email: user.email
+        };
+    }
+
+    @Post('login')
+    async login(@Body() loginDto: LoginDto) {
+        const user = await this.authService.validateUser(loginDto.email, loginDto.password);
+        if (!user) {
+            return { message: 'User not found or password does not match.' };
+        }
+        return this.authService.login(user);
+    }
 }
