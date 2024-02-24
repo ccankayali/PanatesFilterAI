fb9165cf6f27cac7c8a142a8d590ce936fecc7ef
ccankayali <volkand135@gmail.com>
2024-02-24 17:07:03 +0300

    post service  //full

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,30 +1,45 @@
 /* eslint-disable prettier/prettier */
-
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Post, PostDocument } from './entities/post.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
-    export class PostService {
-        constructor(@InjectModel(Post.name) private postModel: Model<PostDocument>) {}
-    
+export class PostService {
+    constructor(@InjectModel(Post.name) private postModel: Model<PostDocument>) {}
+
     async create(createPostDto: CreatePostDto): Promise<Post> {
-        const createdPost = new this.postModel(createPostDto);
+        const newId = await this.getNextUniqueId();
+        const createdPost = new this.postModel({
+            ...createPostDto,
+            id: newId,
+        });
         return createdPost.save();
+    }
+
+    private async getNextUniqueId(): Promise<number> {
+        const lastPost = await this.postModel.findOne().sort({ id: -1 }).exec();
+        return lastPost && lastPost.id ? lastPost.id + 1 : 1;
     }
 
     async findAll(): Promise<Post[]> {
         return this.postModel.find().exec();
     }
 
-    async update(id: string, updatePostDto: CreatePostDto): Promise<Post> {
-        return this.postModel.findByIdAndUpdate(id, updatePostDto, { new: true }).exec();
+    async update(id: number, updatePostDto: CreatePostDto): Promise<Post> {
+        const updatedPost = await this.postModel.findOneAndUpdate({ id }, updatePostDto, { new: true }).exec();
+        if (!updatedPost) {
+            throw new NotFoundException(`Post with ID "${id}" not found.`);
+        }
+        return updatedPost;
     }
 
-    async delete(id: string): Promise<Post> {
-        return this.postModel.findByIdAndDelete(id).exec();
+    async delete(id: number): Promise<Post> {
+        const deletedPost = await this.postModel.findOneAndDelete({ id }).exec();
+        if (!deletedPost) {
+            throw new NotFoundException(`Post with ID "${id}" not found.`);
+        }
+        return deletedPost;
     }
-
 }
