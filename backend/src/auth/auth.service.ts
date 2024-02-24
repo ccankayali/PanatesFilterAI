dd858767ca1eea4f1ec43492b39c3e04623074a7
ccankayali <volkand135@gmail.com>
2024-02-24 13:23:27 +0300

    authservicvce  //full

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,28 +1,24 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { UsersService } from './usersService';
 
 @Injectable()
 export class AuthService {
-    constructor(
-    private usersService: UsersService,
-    private jwtService: JwtService) {}
+    constructor(private usersService: UsersService) {}
 
-    async validateUser(email:string, pass:string): Promise<any> {
+    async validateUser(email: string, pass: string): Promise<any> {
         const user = await this.usersService.findOne(email);
         if (user && user.password === pass) {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            const { password, ...result } = user;
-            return result;
+            return { email: user.email, id: user.id };
         }
         return null;
     }
 
     async login(user: any) {
-        const payload = { email: user.email, sub: user.userID};
         return {
-            access_token: this.jwtService.sign(payload),
+            message: "Login successful",
+            email: user.email,
+            id: user.id
         };
     }
 }
