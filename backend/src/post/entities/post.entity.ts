74f95132a6dea1100209e6018c063f70dba6e3e2
ccankayali <volkand135@gmail.com>
2024-02-24 17:07:15 +0300

    post entity  //full

--- backend/src/post/entities/post.entity.ts
+++ backend/src/post/entities/post.entity.ts
@@ -1,17 +1,27 @@
 /* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from 'mongoose';
 
-export type PostDocument = Post & Document;
+export type PostDocument = Document & {
+    id: number; // Benzersiz ID
+    title: string;
+    content: string;
+    createdAt: Date;
+};
+
 @Schema()
-    export class Post {
-    @Prop({required: true})
+export class Post {
+    @Prop({ required: true, unique: true })
+    id: number; // Benzersiz ID alanı
+
+    @Prop({ required: true })
     title: string;
 
     @Prop()
     content: string;
 
-    @Prop({default: Date.now})
-    cratedAt: Date;
+    @Prop({ default: Date.now })
+    createdAt: Date;
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
