4198523ec2c701f697596d7c7075682b7984e35c
ccankayali <volkand135@gmail.com>
2024-02-28 00:09:28 +0300

    ai

--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -1,15 +1,15 @@
+/* eslint-disable prettier/prettier */
 // src/post/post.module.ts
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Post, PostSchema } from './entities/post.entity';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
-  ],
-  controllers: [PostController],
-  providers: [PostService],
+    imports: [MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]), HttpModule,],
+    controllers: [PostController],
+    providers: [PostService],
 })
 export class PostModule {}
