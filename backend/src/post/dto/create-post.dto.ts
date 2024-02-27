6d8e1c16f82c10b7714fb634e4d3a210344586ed
ccankayali <volkand135@gmail.com>
2024-02-28 00:09:37 +0300

    ai

--- backend/src/post/dto/create-post.dto.ts
+++ backend/src/post/dto/create-post.dto.ts
@@ -1,12 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { IsNotEmpty, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
 
 export class CreatePostDto {
     @IsNotEmpty()
     @IsString()
-    readonly title: string;
-
-    @IsNotEmpty()
-    @IsString()
     readonly content: string;
 }
+
+export class UpdatePostDto extends PartialType(CreatePostDto) {}
