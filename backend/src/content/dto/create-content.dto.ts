48afa271329435ef3f99d12c27759b5a222839a9
ccankayali <volkand135@gmail.com>
2024-03-04 16:59:02 +0300

    final

--- backend/src/content/dto/create-content.dto.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { SafeSearchResponse } from "../content.service";
-
-export class CreateImageDto {
-  imageUrl: string;
-  analysisResults: SafeSearchResponse;
-}
