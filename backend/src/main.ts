fa7edf8ba85d337824472c0d8f5eb807ad607c39
ccankayali <volkand135@gmail.com>
2024-03-04 23:25:12 +0300

    final

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,14 @@
-/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.enableCors(); // CORS'u etkinleştir
   app.setGlobalPrefix('api');
-  
+
   await app.listen(5000);
-  
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
